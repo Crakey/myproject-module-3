61bcdfe8b8d9929ad2cc693b955bdb03d5924efa
Crakey <43682617+Crakey@users.noreply.github.com>
2018-10-05 16:36:38 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+   <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'my first vue app, wooo :)'
+    }
+  }
+}
+</script>
+
+<style>
+h1{
+  color: #444;
+  font-weight: normal;
+  text-align: center;
+}
+
+</style>
