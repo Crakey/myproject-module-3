77a1080fd5d002183e73a7e8305e85134b980abd
Crakey <43682617+Crakey@users.noreply.github.com>
2018-10-11 17:23:11 -0400

    mod 3

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,43 @@
 <template>
   <div id="app">
    <h1>{{ title }}</h1>
+   <navbar />
+   <AllFriends :friends="friends" @delete="deleteFriend" />
+   <OnlineFriends :friends="friends" />
   </div>
 </template>
 
 <script>
+import Navbar from './Navbar'
+import OnlineFriends from './OnlineFriends'
+import AllFriends from './AllFriends'
+
 export default {
   name: 'app',
+  components: {
+    Navbar,
+    OnlineFriends,
+    AllFriends
+    
+  },
   data () {
     return {
-      title: 'my first vue app, wooo :)'
+      title: 'my first vue app, wooo :)',
+      friends: [
+                { name: 'Mario', online: true },
+                { name: 'Luigi', online: false },
+                { name: 'Toad', online: true },
+                { name: 'Bowser', online: false }
+            ]
+    }
+  },
+  methods: {
+    deleteFriend(payload) {
+      console.log(payload)
+      this.friends = this.friends.filter(friend => {
+        return friend.name !== payload.name
+      })
+
     }
   }
 }
